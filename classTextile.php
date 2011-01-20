14f0d196c8d7d742e034475aab7829c2081507f0
netcarver <nc.github@netcarving.com>
2011-01-20 03:00:15 +0000

    Allow pipe closure of captions for consistency.

--- classTextile.php
+++ classTextile.php
@@ -698,10 +698,12 @@ class Textile
 			$row = ltrim($row);
 
 			// Caption -- can only occur on row 1, otherwise treat '|=. foo |...' as a normal center-aligned cell.
-			if ( ($c_row <= 1) && preg_match("/^\|\=($this->s$this->a$this->c)\. ([^\|\n]*)(.*)/s", ltrim($row), $cmtch)) {
+			if ( ($c_row <= 1) && preg_match("/^\|\=($this->s$this->a$this->c)\. ([^\n]*)(.*)/s", ltrim($row), $cmtch)) {
 				$capts = $this->pba($cmtch[1]);
 				$cap = "\t<caption".$capts.">".trim($cmtch[2])."</caption>\n";
 				$row = ltrim($cmtch[3]);
+				if( empty($row) )
+				  continue;
 			}
 			$c_row += 1;
 
