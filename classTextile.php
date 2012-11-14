0fd0bea5b9711d105a517efb3458b999fa241a94
netcarver <nc.github@netcarving.com>
2012-11-14 19:18:53 +0000

    Adjust comment

--- classTextile.php
+++ classTextile.php
@@ -470,7 +470,7 @@ class Textile
 		);
 
 		$this->glyph_replace = array(
-			'$1$2$3$4'.txt_dimension.'$5$6',           // dimension sign
+			'$1$2$3$4'.txt_dimension.'$5$6',       // dimension sign
 			txt_quote_double_open.'$1'.txt_quote_double_close,  // double quotes around numerics
 			'$1'.txt_prime_double,                 // double prime after numerics
 			txt_quote_single_open.'$1'.txt_quote_single_close,  // single quotes around numerics
