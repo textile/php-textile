5219d550d1bb5f0b29d3e6c98184d1899b063a50
netcarver <nc.github@netcarving.com>
2012-12-02 22:57:18 +0000

    Comments for TextileBag::__call()

--- classTextile.php
+++ classTextile.php
@@ -365,6 +365,18 @@ class TextileBag
 		$this->data = (is_array($initial_data)) ? $initial_data : array();
 	}
 
+	/**
+	 * Allows setting of an element in the $data array. eg...
+	 *
+	 * $bag->key(value);
+	 *
+	 * ...sets $bag's $data['key'] to $value provided $value is not empty.
+	 * The set can be made forced by following $value with true...
+	 *
+	 * $bag->key(value, true);
+	 *
+	 * Would force the value into the data array even if it were empty.
+	 **/
 	function __call($k, $params)
 	{
 		$allow_empty = isset($params[1]) && is_bool($params[1]) ? $params[1] : false;
