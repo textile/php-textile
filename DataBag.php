1ca3a43585d45429b729318e4e2712405537f7e4
netcarver <nc.github@netcarving.com>
2012-12-10 17:27:36 +0000

    Initial work on PSR-0 namespaces

---
+++ DataBag.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Netcarver\Textile;
+
+
+/**
+ * Class to allow simple assignment to members of the internal data array
+ **/
+class DataBag
+{
+    protected $data;
+
+
+    public function __construct($initial_data)
+    {
+        $this->data = (is_array($initial_data)) ? $initial_data : array();
+    }
+
+
+    /**
+     * Allows setting of an element in the $data array. eg...
+     *
+     * $bag->key(value);
+     *
+     * ...sets $bag's $data['key'] to $value provided $value is not empty.
+     * The set can be made forced by following $value with true...
+     *
+     * $bag->key(value, true);
+     *
+     * Would force the value into the data array even if it were empty.
+     **/
+    public function __call($k, $params)
+    {
+        $allow_empty = isset($params[1]) && is_bool($params[1]) ? $params[1] : false;
+        if ($allow_empty || '' != $params[0])
+            $this->data[$k] = $params[0];
+
+        return $this;
+    }
+}
---
+++ Tag.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Netcarver\Textile;
+
+
+/**
+ * Class to allow contruction of HTML tags on conversion of an object to a string
+ *
+ * Example usage...
+ *
+ * $img = new TextileTag('img')->class('big blue')->src('images/elephant.jpg');
+ * echo $img;
+ **/
+class Tag extends Netcarver\Textile\DataBag
+{
+    protected $tag;
+    protected $selfclose;
+
+
+    public function __construct($name, $attribs=array(), $selfclosing=true)
+    {
+        parent::__construct($attribs);
+        $this->tag = $name;
+        $this->selfclose = $selfclosing;
+    }
+
+
+    public function __toString() {
+        $attribs = '';
+
+        if (count($this->data)) {
+            ksort($this->data);
+            foreach ($this->data as $k=>$v)
+                $attribs .= " $k=\"$v\"";
+        }
+
+        if ($this->tag)
+            $o = '<' . $this->tag . $attribs . (($this->selfclose) ? " />" : '>');
+        else
+            $o = $attribs;
+
+        return $o;
+    }
+}
