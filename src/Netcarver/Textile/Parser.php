b9641df447d881aa9fed743b87ca1773606c2e10
netcarver <nc.github@netcarving.com>
2012-12-16 22:14:16 +0000

    Remove horizontal alignement from spans. Closes #66

--- src/Netcarver/Textile/Parser.php
+++ src/Netcarver/Textile/Parser.php
@@ -1617,7 +1617,7 @@ class Parser
                 $text = preg_replace_callback("/
                     (^|(?<=[\s>$pnct\(])|[{[])            # pre
                     ($f)(?!$f)                            # tag
-                    ({$this->c})                          # atts
+                    ({$this->lc})                         # atts - do not use horizontal alignment; it kills html tags within inline elements.
                     (?::(\S+))?                           # cite
                     ([^\s$f]+|\S.*?[^\s$f\n])             # content
                     ([$pnct]*)                            # end
