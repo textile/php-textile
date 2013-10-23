2d22111e084c437b9f7983f108e8b647695ae5aa
Jukka Svahn <web@rahinaa.biz>
2013-10-23 17:41:30 +0300

    Run fixtures using PHPunit.

--- test/Netcarver/Textile/Test/BasicTest.php
+++ test/Netcarver/Textile/Test/BasicTest.php
@@ -1,11 +1,62 @@
 <?php
 
 namespace Netcarver\Textile\Test;
+use Symfony\Component\Yaml\Yaml;
 
 class BasicTest extends \PHPUnit_Framework_TestCase
 {
-    public function testExample()
+    /**
+     * @dataProvider provider
+     */
+
+    public function testAdd($expect, $input, $name)
     {
-        $this->assertTrue(true);
+        $this->assertEquals($expect, $input, 'In section: '.$name);
+    }
+ 
+    public function provider()
+    {
+        chdir(dirname(dirname(dirname(__DIR__))));
+
+        $yaml = Yaml::parse('basic.yaml');
+
+        foreach ($yaml as $name => $test) {
+            if (!isset($test['input']) || !isset($test['expect'])) {
+                continue;
+            }
+
+            if (isset($test['assert']) && $test['assert'] === 'skip') {
+                continue;
+            }
+
+            $textile = new \Netcarver\Textile\Parser();
+
+            if (isset($test['setup'][0]))
+            {
+                foreach ($test['setup'][0] as $method => $value)
+                {
+                    $textile->$method($value);
+                }
+            }
+
+            $expect = rtrim($test['expect']);
+            $input = $textile->textileThis($test['input']);
+
+            foreach (array('expect', 'input') as $variable)
+            {
+                $$variable = preg_replace(
+                    array(
+                        '/ id="(fn|note)[a-z0-9]*"/',
+                        '/ href="#(fn|note)[a-z0-9]*"/',
+                    ),
+                    '',
+                    $$variable
+                );
+            }
+
+            $out[] = array($expect, $input, $name);
+        }
+
+        return $out;
     }
 }
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,6 +2,13 @@
 
 error_reporting(E_ALL);
 
+/*
 include dirname(__DIR__) . '/src/Netcarver/Textile/Parser.php';
 include dirname(__DIR__) . '/src/Netcarver/Textile/DataBag.php';
 include dirname(__DIR__) . '/src/Netcarver/Textile/Tag.php';
+*/
+
+include dirname(__DIR__) . '/vendor/autoload.php';
+$loader = new \Composer\Autoload\ClassLoader();
+$loader->add('Netcarver\\Textile\\Test\\', __DIR__);
+$loader->register();
