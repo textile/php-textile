02311e55370d02f6ad3468a016d7236ae305ab6d
Jukka Svahn <web@rahinaa.biz>
2013-10-23 10:24:27 +0300

    Base PHPunit configuration.
    For users and Travis. Stops Travis from failing for now
    too.

---
+++ test/Netcarver/Textile/Test/BasicTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Netcarver\Textile\Test;
+
+class BasicTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ test/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+error_reporting(E_ALL);
+
+include dirname(__DIR__) . '/src/Netcarver/Textile/Parser.php';
+include dirname(__DIR__) . '/src/Netcarver/Textile/Databag.php';
+include dirname(__DIR__) . '/src/Netcarver/Textile/Tag.php';
