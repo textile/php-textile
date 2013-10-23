60ce4a50a419f0ad9bc973a19487283ff55f4745
Jukka Svahn <web@rahinaa.biz>
2013-10-23 10:28:23 +0300

    Case-sensitive filesystems.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,5 +3,5 @@
 error_reporting(E_ALL);
 
 include dirname(__DIR__) . '/src/Netcarver/Textile/Parser.php';
-include dirname(__DIR__) . '/src/Netcarver/Textile/Databag.php';
+include dirname(__DIR__) . '/src/Netcarver/Textile/DataBag.php';
 include dirname(__DIR__) . '/src/Netcarver/Textile/Tag.php';
