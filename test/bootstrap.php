c9a84d2414947788b7c3be6f508412ffcf7d3588
Jukka Svahn <web@rahinaa.biz>
2013-11-01 13:33:00 +0200

    Remove commented lines from test bootstrap.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,13 +1,6 @@
 <?php
 
 error_reporting(E_ALL);
-
-/*
-include dirname(__DIR__) . '/src/Netcarver/Textile/Parser.php';
-include dirname(__DIR__) . '/src/Netcarver/Textile/DataBag.php';
-include dirname(__DIR__) . '/src/Netcarver/Textile/Tag.php';
-*/
-
 include dirname(__DIR__) . '/vendor/autoload.php';
 $loader = new \Composer\Autoload\ClassLoader();
 $loader->add('Netcarver\\Textile\\Test\\', __DIR__);
