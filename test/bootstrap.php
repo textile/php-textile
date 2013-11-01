d4ca69cea79a25a4de48fdd8090be0f8883150f7
Jukka Svahn <web@rahinaa.biz>
2013-11-01 14:04:04 +0200

    Capture suppressed errors.
    Tests do not pass if there are suppressed errors.
    Error suppression isn't valid practice. It should merely
    be used to normalize differences between old PHP
    releases that might throw warnings instead of
    exceptions or booleans.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,7 +1,41 @@
 <?php
 
+namespace Netcarver\Textile\Test;
+
 error_reporting(E_ALL);
-include dirname(__DIR__) . '/vendor/autoload.php';
-$loader = new \Composer\Autoload\ClassLoader();
-$loader->add('Netcarver\\Textile\\Test\\', __DIR__);
-$loader->register();
+
+/**
+ * Initialises tests.
+ */
+
+class Bootstrap
+{
+    /**
+     * Constructor.
+     */
+
+    public function __construct()
+    {
+        set_error_handler(array($this, 'errorHandler'));
+        include dirname(__DIR__) . '/vendor/autoload.php';
+        $loader = new \Composer\Autoload\ClassLoader();
+        $loader->add('Netcarver\\Textile\\Test\\', __DIR__);
+        $loader->register();
+    }
+
+    /**
+     * Error handler.
+     *
+     * @param int    $errno
+     * @param string $errstr
+     * @param string $errfile
+     * @param int    $errline
+     */
+
+    public function errorHandler($errno, $errstr, $errfile, $errline)
+    {
+        throw new \Exception($errstr . ' in ' . $errfile . ' on line ' . $errline);
+    }
+}
+
+new Bootstrap();
