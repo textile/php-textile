c4f00063f74957f23c578c4b3b8ff670d5dc527e
Jukka Svahn <web@rahinaa.biz>
2013-12-17 13:44:13 +0200

    Isn't run on PHP 5.3.0.
    Lambdas it is then.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,38 +4,11 @@ namespace Netcarver\Textile\Test;
 
 error_reporting(E_ALL);
 
-/**
- * Initialises tests.
- */
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    throw new \Exception($errstr . ' in ' . $errfile . ' on line ' . $errline);
+});
 
-class Bootstrap
-{
-    /**
-     * Constructor.
-     */
-
-    public function __construct()
-    {
-        set_error_handler(array($this, 'errorHandler'));
-        include dirname(__DIR__) . '/vendor/autoload.php';
-        $loader = new \Composer\Autoload\ClassLoader();
-        $loader->add('Netcarver\\Textile\\Test\\', __DIR__);
-        $loader->register();
-    }
-
-    /**
-     * Error handler.
-     *
-     * @param int    $errno
-     * @param string $errstr
-     * @param string $errfile
-     * @param int    $errline
-     */
-
-    public function errorHandler($errno, $errstr, $errfile, $errline)
-    {
-        throw new \Exception($errstr . ' in ' . $errfile . ' on line ' . $errline);
-    }
-}
-
-new Bootstrap();
+include dirname(__DIR__) . '/vendor/autoload.php';
+$loader = new \Composer\Autoload\ClassLoader();
+$loader->add('Netcarver\\Textile\\Test\\', __DIR__);
+$loader->register();
