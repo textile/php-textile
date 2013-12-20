964e8c9666accad92e0d0d4eaaa518e75747c3be
Jukka Svahn <web@rahinaa.biz>
2013-12-20 17:40:36 +0200

    Affects exception testing.
    There is no much of error suppression in the codebase
    either.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,10 +6,6 @@ ini_set('memory_limit', '512M');
 
 error_reporting(E_ALL);
 
-set_error_handler(function ($errno, $errstr, $errfile, $errline) {
-    throw new \Exception($errstr . ' in ' . $errfile . ' on line ' . $errline);
-});
-
 include dirname(__DIR__) . '/vendor/autoload.php';
 $loader = new \Composer\Autoload\ClassLoader();
 $loader->add('Netcarver\\Textile\\Test\\', __DIR__);
