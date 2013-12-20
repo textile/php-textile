312291463b13b29f076cde8d82c4d05157dc9910
Jukka Svahn <web@rahinaa.biz>
2013-12-20 17:20:40 +0200

    Test coverage on 2.5.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,37 +2,12 @@
 
 namespace Netcarver\Textile\Test;
 
+ini_set('memory_limit', '512M');
+
 error_reporting(E_ALL);
 
-/**
- * Initialises tests.
- */
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    throw new \Exception($errstr . ' in ' . $errfile . ' on line ' . $errline);
+});
 
-class Bootstrap
-{
-    /**
-     * Constructor.
-     */
-
-    public function __construct()
-    {
-        set_error_handler(array($this, 'errorHandler'));
-        include dirname(__DIR__) . '/classTextile.php';
-    }
-
-    /**
-     * Error handler.
-     *
-     * @param int    $errno
-     * @param string $errstr
-     * @param string $errfile
-     * @param int    $errline
-     */
-
-    public function errorHandler($errno, $errstr, $errfile, $errline)
-    {
-        throw new \Exception($errstr . ' in ' . $errfile . ' on line ' . $errline);
-    }
-}
-
-new Bootstrap();
+include dirname(__DIR__) . '/classTextile.php';
