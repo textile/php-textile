bb6ecdf8f353fae0f9937bd4e6fba35f44bb99a4
Jukka Svahn <void@rah.pw>
2018-11-20 12:45:42 +0200

    Use autoload-dev

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,12 +1,5 @@
 <?php
 
-namespace Netcarver\Textile\Test;
-
 ini_set('memory_limit', '512M');
-
 error_reporting(E_ALL);
-
-include dirname(__DIR__) . '/vendor/autoload.php';
-$loader = new \Composer\Autoload\ClassLoader();
-$loader->add('Netcarver\\Textile\\Test\\', __DIR__);
-$loader->register();
+require dirname(__DIR__) . '/vendor/autoload.php';
